3031a790863bc936a7d3fd975e8e30eb5c001ccd
Zainds <beat1097@mail.ru>
2025-03-14 19:24:02 +0700

    improve project structure

--- lab2/obj/Debug/net9.0/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net9.0/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c8a4c6994bcffeb0a73a5d05e0231319016a4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
